4dfaf9c70030498675e984f45b6935744942ad23
samedfft2634 <samedfft@gmail.com>
2023-11-08 00:44:31 +0300

    check and trash

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const addBtn = document.querySelector(".addBtn");
-const todoListul  = document.querySelector(".tasks ul");
+const todoListul = document.querySelector(".tasks ul");
 const inputValue = document.getElementById("input");
+//
+const checked = document.querySelector(".checked");
+const total = document.querySelector(".total");
+let checkedTask = 0;
+let totalTask = 0;
 
 /* ==================== date and time =================== */
 const dateTime = document.querySelector(".date-time");
 let year = new Date().getFullYear();
-let month = new Date().getMonth()+1;
+let month = new Date().getMonth() + 1;
 let day = new Date().getDate();
 let date = `${day}/${month}/${year}`;
 let spanDate = document.createElement("span");
@@ -22,6 +27,7 @@ spanTime.textContent = time;
 
 addBtn.addEventListener("click", () => {
 	addItem();
+	total.textContent = totalTask;
 });
 
 const addItem = () => {
@@ -34,7 +40,7 @@ const addItem = () => {
 	} else {
 		let createLi = document.createElement("li");
 		createLi.className = "createLi";
-		todoListul .appendChild(createLi);
+		todoListul.appendChild(createLi);
 		createLi.innerHTML = `
         <div class="checkbox-wrapper-39">
         <label>
@@ -42,21 +48,40 @@ const addItem = () => {
          <span class="checkbox"></span>
          </label>
         </div>
-
         <span class="todoText">${inputValue.value}</span> <i style="display:none" class="fa-solid fa-trash-can"></i> `;
 		inputValue.value = "";
+		++totalTask;
 	}
 };
 
+todoListul.addEventListener("change", (e) => {
+	let li = e.target.closest("li").querySelector(".todoText");
+	const removeBtn = e.target.closest("li").querySelector(".fa-trash-can");
+	if (e.target.checked) {
+		li.style.textDecoration = "line-through";
+		removeBtn.style.display = "flex";
+        ++checkedTask
+	} else if ((e.target = "checkbox-wrapper-39")) {
+		li.style.textDecoration = "none";
+		removeBtn.style.display = "none";
+        --checkedTask
+	}
+    checked.textContent = checkedTask
+});
 
-todoListul.addEventListener("change",(e)=>{
-    let li = e.target.closest("li").querySelector(".todoText")
-    const removeBtn = e.target.closest("li").querySelector(".fa-trash-can");
-    if(e.target.checked){
-        li.style.textDecoration = "line-through"
-        removeBtn.style.display = "flex";
-    } else if(e.target = "checkbox-wrapper-39" ){
-        li.style.textDecoration = "none"
-        removeBtn.style.display = "none";
-    }
-})
+
+todoListul.addEventListener("click", (e) => {
+	let delBtn = e.target.closest("li").querySelector(".fa-trash-can");
+	if (e.target == delBtn) {
+		let li = e.target.closest("li");
+		li.classList.add("exit-animation");
+        --totalTask;
+        --checkedTask
+		setTimeout(() => {
+			li.remove();
+			todoListul.removeChild(li);	
+		}, 2000);
+	}
+    checked.textContent = checkedTask
+    total.textContent = totalTask;
+});
