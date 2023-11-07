106f0c9dd4afc5f47840caa34a6a8c4aac0508e0
samedfft2634 <samedfft@gmail.com>
2023-11-08 00:04:01 +0300

    new js new styles

--- app.js
+++ app.js
@@ -1,47 +1,62 @@
 const addBtn = document.querySelector(".addBtn");
-const todoUl = document.querySelector(".tasks ul");
+const todoListul  = document.querySelector(".tasks ul");
 const inputValue = document.getElementById("input");
 
-
 /* ==================== date and time =================== */
-// const dateTime = document.querySelector(".date-time");
-// let year = new Date().getFullYear();
-// let month = new Date().getMonth()+1;
-// let day = new Date().getDate();
-// let date = `${day}/${month}/${year}`;
-// let spanDate = document.createElement("span");
-// dateTime.appendChild(spanDate);
-// spanDate.textContent = date;
+const dateTime = document.querySelector(".date-time");
+let year = new Date().getFullYear();
+let month = new Date().getMonth()+1;
+let day = new Date().getDate();
+let date = `${day}/${month}/${year}`;
+let spanDate = document.createElement("span");
+dateTime.appendChild(spanDate);
+spanDate.textContent = date;
 
-// let hour = new Date().getHours();
-// let minute = new Date().getMinutes();
-// let time = `${hour} : ${minute}`;
-// let spanTime = document.createElement("span");
-// dateTime.appendChild(spanTime);
-// spanTime.textContent = time;
+let hour = new Date().getHours();
+let minute = new Date().getMinutes();
+let time = `${hour} : ${minute}`;
+let spanTime = document.createElement("span");
+dateTime.appendChild(spanTime);
+spanTime.textContent = time;
 /* ==================== date and time =================== */
 
-addBtn.addEventListener("click",()=>{
-    addItem()
-})
+addBtn.addEventListener("click", () => {
+	addItem();
+});
 
 const addItem = () => {
-    if(!inputValue.value){
-        Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "The input field cannot be empty.",
-    });
-    } else {
-        let createLi = document.createElement("li");
-        createLi.className = "createLi"
-        todoUl.appendChild(createLi)
-        createLi.innerHTML = `
-        <div class="checkbox-wrapper-11">
-        <input id="02-11" type="checkbox" name="r" value="2">
-        <label for="02-11">${inputValue.value}</label>
-        </div>  
-        <i style="display:none" class="fa-solid fa-trash-can"></i> `;
-        inputValue.value = "";
+	if (!inputValue.value) {
+		Swal.fire({
+			icon: "error",
+			title: "Oops...",
+			text: "The input field cannot be empty.",
+		});
+	} else {
+		let createLi = document.createElement("li");
+		createLi.className = "createLi";
+		todoListul .appendChild(createLi);
+		createLi.innerHTML = `
+        <div class="checkbox-wrapper-39">
+        <label>
+         <input type="checkbox"/>
+         <span class="checkbox"></span>
+         </label>
+        </div>
+
+        <span class="todoText">${inputValue.value}</span> <i style="display:none" class="fa-solid fa-trash-can"></i> `;
+		inputValue.value = "";
+	}
+};
+
+
+todoListul.addEventListener("change",(e)=>{
+    let li = e.target.closest("li").querySelector(".todoText")
+    const removeBtn = e.target.closest("li").querySelector(".fa-trash-can");
+    if(e.target.checked){
+        li.style.textDecoration = "line-through"
+        removeBtn.style.display = "flex";
+    } else if(e.target = "checkbox-wrapper-39" ){
+        li.style.textDecoration = "none"
+        removeBtn.style.display = "none";
     }
-}
+})
