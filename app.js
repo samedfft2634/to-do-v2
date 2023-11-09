d6281915ca96b7d6b825d46e7ac627238e8a363d
samedfft2634 <samedfft@gmail.com>
2023-11-09 20:03:00 +0300

    project done

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const addBtn = document.querySelector(".addBtn");
 const todoListul = document.querySelector(".tasks ul");
 const inputValue = document.getElementById("input");
 //
-const checked = document.querySelector(".checked");
-const total = document.querySelector(".total");
+const createLi = document.querySelectorAll(".createLi");
+
+let todos = [];
 
 /* ==================== date and time =================== */
 const dateTime = document.querySelector(".date-time");
@@ -23,92 +24,143 @@ spanDate.textContent = date;
 // spanTime.textContent = time;
 /* ==================== date and time =================== */
 
-addBtn.addEventListener("click", () => {
-	addItem();
-});
-
-const addItem = () => {
-	if (!inputValue.value) {
+const addItem = (e) => {
+	if (!inputValue.value.trim()) {
 		swal();
+	} else if (
+		todos.some(
+			(todo) => todo.text === inputValue.value.trim().toLowerCase()
+		)
+	) {
+		Swal.fire({
+			icon: "warning",
+			title: "Duplicate Task",
+			text: "This task already exists!",
+		});
 	} else {
 		let createLi = document.createElement("li");
 		createLi.className = "createLi";
 		todoListul.appendChild(createLi);
 		createLi.innerHTML = `
-        <div class="checkbox-wrapper-39">
-        <label>
-         <input type="checkbox"/>
-         <span class="checkbox"></span>
-         </label>
-        </div>
-        <span class="todoText">${inputValue.value}</span> <i style="display:none" class="fa-solid fa-trash-can"></i> `;
-		inputValue.value = "";
+		<div class="checkbox-wrapper-39">
+	  <label>
+		<input type="checkbox"  class ="checkbx" />
+		<span class="checkbox"></span>
+	  </label>
+	</div>
+	   <span class="todoText">${inputValue.value}</span> <i id="remove" style="display:none" class="fa-solid fa-trash-can"></i>  `;
+		if ((e.target = "fa-plus")) {
+			todos.push({
+				text: inputValue.value,
+				cheq: false,
+				line: false,
+				trash: false,
+			});
+			localStorage.setItem("todos", JSON.stringify(todos));
+			inputValue.value = "";
+		}
 	}
 };
+addBtn.addEventListener("click", addItem);
 
+//
 todoListul.addEventListener("change", (e) => {
-	let li = e.target.closest("li").querySelector(".todoText");
-	const removeBtn = e.target.closest("li").querySelector(".fa-trash-can");
-	if (e.target.checked) {
-		li.style.textDecoration = "line-through";
-		removeBtn.style.display = "flex";
-	} else if ((e.target = "checkbox-wrapper-39")) {
-		li.style.textDecoration = "none";
-		removeBtn.style.display = "none";
+	if (e.target.classList.contains("checkbx")) {
+		const liElement = e.target.closest("li");
+		const liText = liElement.querySelector(".todoText");
+		const removeBtn = liElement.querySelector(".fa-trash-can");
+
+		const index = Array.from(todoListul.children).indexOf(liElement);
+
+		todos[index].line = e.target.checked;
+		todos[index].trash = e.target.checked;
+
+		if (e.target.checked) {
+			liText.style.textDecoration = todos[index].line
+				? "line-through"
+				: "none";
+			liText.style.color = "gray";
+			removeBtn.style.display = todos[index].trash ? "flex" : "none";
+			removeBtn.style.cursor = "pointer";
+			todos[index].cheq = true;
+		} else {
+			liText.style.color = "rgb(247, 226, 223)";
+			liText.style.textDecoration = todos[index].line
+				? "none"
+				: "line-through";
+			liText.style.textDecoration = "none";
+			removeBtn.style.display = todos[index].trash ? "flex" : "none";
+			todos[index].cheq = false;
+		}
 	}
+	localStorage.setItem("todos", JSON.stringify(todos));
 });
 
 todoListul.addEventListener("click", (e) => {
-	let delBtn = e.target.closest("li").querySelector(".fa-trash-can");
-	if (e.target == delBtn) {
-		let li = e.target.closest("li");
-		li.classList.add("exit-animation");
-		setTimeout(() => {
-			li.remove();
-			todoListul.removeChild(li);
-		}, 2000);
+	const removeLi = e.target.closest("li");
+	if (e.target.id === "remove") {
+		const index = Array.from(todoListul.children).indexOf(removeLi);
+		todos.splice(index, 1);
+		removeLi.remove();
 	}
+	localStorage.setItem("todos", JSON.stringify(todos));
 });
 
 document.querySelector("body").addEventListener("keyup", (e) => {
+	inputValue.focus()
 	if (e.key === "Enter") {
-		if (!inputValue.value && !inputValue.classList.contains("empty")) {
-			inputValue.focus();
-			inputValue.classList.add("empty");
-		} else if (
-			!inputValue.value &&
-			inputValue.classList.contains("empty")
-		) {
-			swal();
-			inputValue.classList.remove("empty");
-		} else if (inputValue.value) {
-			addItem();
-		}
-	} else if (e.key === "Delete") {
-		del();
-	}
-
+		addItem(e);
+	} 
 });
 
 const swal = () => {
 	Swal.fire({
-		icon: "error",
-		title: "Oops...",
-		text: "The input field cannot be empty.",
-	});
+		position: "center",
+		icon: "warning",
+		title: "The input field cannot be empty!",
+		showConfirmButton: false,
+		timer: 1100
+	  });
 };
 
-const del = () => {
-	let li = document.querySelector(".createLi");
-	if (li && !inputValue.value) {
-		todoListul.lastElementChild.remove();
-	} else {
-		Swal.fire({
-			position: "center",
-			icon: "question",
-			title: "There is no task to delete!",
-			showConfirmButton: false,
-			timer: 500,
+
+// const del = () => {
+// 	let li = document.querySelector(".createLi");
+// 	if (li && !inputValue.value) {
+// 		todoListul.lastElementChild.remove();
+// 	} else {
+// 		Swal.fire({
+// 			position: "center",
+// 			icon: "question",
+// 			title: "There is no task to delete!",
+// 			showConfirmButton: false,
+// 			timer: 500,
+// 		});
+// 	}
+// };
+
+function showList() {
+	const savedTodos = localStorage.getItem("todos", todos);
+	if (savedTodos) {
+		todos = JSON.parse(savedTodos);
+		todos.forEach((todoItem) => {
+			const li = document.createElement("li");
+			li.className = "createLi";
+			li.innerHTML = `
+		<div class="checkbox-wrapper-39">
+		<label>
+		  <input type="checkbox" class ="checkbx" ${todoItem.cheq ? "checked" : ""} />
+		  <span class="checkbox" "></span>
+		</label>
+	  </div>
+		 <span class="todoText" style="text-decoration:${
+				todoItem.line ? "line-through" : "none"
+			} ">
+		 ${todoItem.text}</span> <i id="remove" style="display: ${
+				todoItem.trash ? "flex" : "none"
+			}" class="fa-solid fa-trash-can"></i>`;
+			todoListul.appendChild(li);
 		});
 	}
 }
+showList();
